0ff7e11e42baf6ef5513fdd6d27198fde5620e56
Varda Quraishi <vardaquraishi.vq@gmail.com>
2020-12-04 00:28:59 +0500

    All fonts added

--- App.js
+++ App.js
@@ -20,7 +20,16 @@ import {Fonts} from './src/utils/Fonts'
 const App = () => {
   return (
     <View style = {{alignItems: 'center', justifyContent: 'center'}}>
+      <Text style = {{fontFamily: Fonts.OSB}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSBi}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSeB}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSeBi}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSI}}>Hello World</Text>
       <Text style = {{fontFamily: Fonts.OSL}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSLi}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSR}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSsB}}>Hello World</Text>
+      <Text style = {{fontFamily: Fonts.OSsBi}}>Hello World</Text>
     </View>
 
   );
