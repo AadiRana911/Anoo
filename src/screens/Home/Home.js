df320369ef463b3fe10c71269cf3457d1326394d
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-12-04 00:43:43 +0500

    Made Home Screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import { View, Text } from 'react-native'
-
+import {Fonts} from '../../utils/Fonts'
 const Home = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style = {{fontSize: 25, fontFamily: Fonts.OSeBi}}>Home Screen</Text>
         </View>
     )
 }
