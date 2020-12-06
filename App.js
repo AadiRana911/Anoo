8fa57354373e3a285114f764b0252c9532ca87d0
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-12-06 15:22:16 +0500

    Nothing

--- App.js
+++ App.js
@@ -9,10 +9,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {Fonts} from './src/utils/Fonts';
-
+import {colors} from './src/theme'
 import Splash from './src/screens/Splash';
 import Home from './src/screens/Home';
 import Topic from './src/screens/Topic'
@@ -21,6 +21,7 @@ import SignUp from './src/screens/SignUp';
 import Login from './src/screens/Login';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
@@ -31,13 +32,60 @@ const App = () => {
         <Stack.Screen name="Home" component={Home} options = {{headerShown: false}} />
         <Stack.Screen name="Topic" component={Topic} options = {{headerShown: false}} />
         <Stack.Screen name="Industry" component={Industry} options = {{headerShown: false}} />
-        <Stack.Screen name="SignUp" component={SignUp} options = {{headerShown: false}} />
-        <Stack.Screen name="Login" component={Login} options = {{headerShown: false}} />
-
+        <Stack.Screen name="Tabs" component={Tabs} options = {{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const LoginStack = () => {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options = {{headerShown: false}} />
+      </Stack.Navigator>
+  )
+}
+
+const SignupStack = () => {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="SignUp" component={SignUp} options = {{headerShown: false}} />
+      </Stack.Navigator>
+  )
+}
+
+const Tabs = () => {
+  return (
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Phone') {
+            iconName = focused
+              ? 'stopwatch'
+              : 'stopwatch';
+          } else if (route.name === 'Email') {
+            iconName = focused ? 'calendar' : 'calendar';
+          } 
+          if (route.name === 'Phone') {
+              return <Entypo name = {iconName} color = {color} size = {size}/>
+          } else if (route.name === 'Email') {
+              return <Foundation name = {iconName} color = {color} size = {size}/>
+          }
+
+          // You can return any component that you like here!
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: colors.primary,
+        inactiveTintColor: 'gray',
+      }}
+    >
+          <Tab.Screen name="Login" component={LoginStack} />
+          <Tab.Screen name="Signup" component={SignupStack} />
+      </Tab.Navigator>
+  )
+}
 
 export default App;
